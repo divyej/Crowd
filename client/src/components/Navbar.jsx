1babf4790f34861df043c8312ba8f44462f34b11
Divye <djain94314@gmail.com>
2023-05-20 01:04:41 +0530

    added context to nav

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -4,13 +4,13 @@ import { Link,useNavigate } from 'react-router-dom'
 import {logo,menu,search,thirdweb} from '../assets'
 import {navlinks} from '../constants'
 import CustomButton from './customButton'
+import { useStateContext } from '../context'
 
-
-const address= '0x1234567890123456789012345678901234567890'
 const Navbar = () => {
     const navigate = useNavigate();
     const [isActive,setIsActive] = useState('dashboard')
     const [toggle,setToggle]=useState(false)
+    const {address,connect} = useStateContext()
   return (
     <div className="flex md:flex-row flex-col-reverse justify-between mb-[35px] gap-6">
         <div className="lg:flex-1 flex flex-row max-w-[458px] py-2 pl-4 pr-2 h-[52px] bg-[#1c1c24] rounded-[100px]">
