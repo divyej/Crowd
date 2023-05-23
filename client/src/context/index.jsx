d968a93aaed0ebc1e3995a856b959ab29fd75a5b
Divye <djain94314@gmail.com>
2023-05-24 01:29:53 +0530

    context

--- client/src/context/index.jsx
+++ client/src/context/index.jsx
@@ -32,9 +32,29 @@ catch (error) {
 }
   }
   const getCampaigns=async()=>{
-    const campaigns = await contract.call(getCampaigns)
-     
-  }
+    const campaigns = await contract.call('getCampaigns')
+
+    const parsedCampaigns =campaigns.map((campaign,i)=>({
+        owner:campaign.owner,
+        title:campaign.title,
+        description:campaign.description,
+        target:ethers.utils.formatEther(campaign.target.toString()),
+        deadline:campaign.deadline.toNumber(),
+        amountCollected: ethers.utils.formatEther(campaign.amountCollected.toString()),
+        image:campaign.image,
+        pId:i
+    })
+  )
+  return parsedCampaigns
+}
+const getUserCampaigns=async()=>{
+    const allCampaigns = await getCampaigns()
+    const userCampaigns = allCampaigns.filter((campaign)=>campaign.owner===address)
+    return userCampaigns
+}
+
+
+
 
 
     return(
