3809ff0d1fad215c4ce2cead91792d512c91eeb5
Divye <djain94314@gmail.com>
2023-05-24 02:41:55 +0530

    added display

--- client/src/components/DisplayCampaigns.jsx
+++ client/src/components/DisplayCampaigns.jsx
@@ -1,8 +1,25 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
+import Loader from './loader'
+const DisplayCampaigns = ({title,isLoading,campaigns}) => {
 
-const DisplayCampaigns = () => {
+    const navigate = useNavigate()
+    
   return (
-    <div>DisplayCampaign </div>
+    <div> 
+        <h1 className='font-epilogue font-semibold text-[18px] text-white text-left'>{title}</h1>
+        <div className='flex flex-wrap gap-[25px] mt-[20px]'>
+            {isLoading && <Loader />}
+            {!isLoading && campaigns.length>0 && campaigns.map((campaign,i)=><FundCard key={campaign.id} {...campaign} handleClick={()=>handleNavigate(campaign) }
+            />)}
+
+          
+    
+    </div>
+    
+    </div>
+
+
   )
 }
 
