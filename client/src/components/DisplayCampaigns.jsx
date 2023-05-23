470a274435531c645e016e46e66e9ac840c1a86b
Divye <djain94314@gmail.com>
2023-05-24 02:21:40 +0530

    home

---
+++ client/src/components/DisplayCampaigns.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const DisplayCampaigns = () => {
+  return (
+    <div>DisplayCampaign </div>
+  )
+}
+
+export default DisplayCampaigns
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,8 +1,33 @@
 import React from 'react'
+import { useState,useEffect } from 'react'
+import { useStateContext } from '../context'
+import DisplayCampaigns from '../components/DisplayCampaigns'
+
 
 const Home = () => {
+    const [isLoading,setIsLoading]=useState(false)
+    const [campaigns,setCampaigns]=useState([])
+    const {address,contract,getCampaigns}= useStateContext()
+
+    const loadCampaigns=async()=>{
+        setIsLoading(true)
+        const data = await getCampaigns()
+        setCampaigns(data)
+        setIsLoading(false)
+    }
+
+    useEffect(()=>{ 
+        if(contract) loadCampaigns()
+    },[address,contract])
+
+
   return (
-    <div>Home</div>
+    <DisplayCampaigns 
+    title='All Campaigns'
+    campaigns={campaigns}
+    isLoading={isLoading}
+    />
+
   )
 }
 
