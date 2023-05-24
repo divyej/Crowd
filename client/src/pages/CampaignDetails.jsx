ababc91d9461393f0bd1090be184133b9b13eeb1
Divye <djain94314@gmail.com>
2023-05-25 00:46:55 +0530

    added more styles

--- client/src/pages/CampaignDetails.jsx
+++ client/src/pages/CampaignDetails.jsx
@@ -58,11 +58,24 @@ const handleDonate=async()=>{
                         <div className='w-[52px] h-[52px] flex items-center justify-center rounded-full bg-[#2c2f32] cursor-pointer'>
                             <img src={thirdweb} alt='user' className='w-[60%] h-[60%] object-contain'/> 
                         </div>
+                      
+                        <h4 className='font-eplogue font-semibold text-[14px] text-white break-all'>{state.owner}</h4>
+                        <p className='mt-[4px] font-epilogue font-normal text-[12px] text-[#808191] '>10 campaigns</p>
                     </div>
                 </div>
             </div>
+            <div>
+            <h4 className ='font-epilogue font-semibold text-[18px] text-white uppercase '>Story</h4>
+            <div className='mt-[20px]'>
+                <p className='font-epilogue font-normal text-[14px] text-[#808191] leading-[26px] text-justify'>{state.description}</p>
+                </div>
+                </div>
+
+
+
+            </div>
         </div>
-    </div>
+    
   )
 }
 
